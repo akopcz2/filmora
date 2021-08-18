f952acf0977827d755c6d5273a042fe7b44ffed4
akopcz2 <akopcz2@gmail.com>
2021-08-17 22:25:00 -0500

    Create chocolateyinstall.ps1

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,14 @@
+$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe'
+  url           = 'https://download.wondershare.com/filmora_full846.exe'
+  softwareName  = 'filmora*'
+  silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
