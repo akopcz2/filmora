aa919efd9de76bd2107fa1c4589bd4e6e78c44d2
akopcz2 <akopcz2@gmail.com>
2021-08-18 09:02:11 -0500

    Update chocolateyinstall.ps1

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,16 +1,6 @@
-$ErrorActionPreference = 'Stop';
-$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$name = 'Filmora'
+$installerType = 'exe'
+$url  = 'https://download.wondershare.com/filmora_full846.exe'
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
 
-$packageArgs = @{
-  packageName   = $env:ChocolateyPackageName
-  unzipLocation = $toolsDir
-  fileType      = 'exe'
-  url           = 'https://download.wondershare.com/filmora_full846.exe'
-  checksum      = 'd87b0f936fca9ff07789fe68553f0015'
-  checsumType   = 'md5'
-  softwareName  = 'filmora*'
-  silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
-  validExitCodes= @(0)
-}
-
-Install-ChocolateyPackage @packageArgs
+Install-ChocolateyPackage $name $installerType $silentArgs $url
